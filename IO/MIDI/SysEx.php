8c861e8700e1126f0b062d1a15dbe1609b58dd30
Yoshihiro Yamazaki <yoya@awm.jp>
2015-03-10 22:39:32 +0900

    SysEx files Plug&Play.

--- IO/MIDI/SysEx.php
+++ IO/MIDI/SysEx.php
@@ -4,15 +4,29 @@
    */
 
 // require_once 'IO/Bit.php';
-require_once('IO/MIDI/SysEx/NSX39.php');
 
 class IO_MIDI_SysEx {
     function dumpPayload($payload, $opts = array()) {
-        $sysExPrefix = IO_MIDI_SysEx_NSX39::SYSEX_PREFIX;
-        $sysExPrefixLen = strlen($sysExPrefix);
-    	if (strncmp($payload, $sysExPrefix, $sysExPrefixLen) === 0) {
-            $nsx39 = new IO_MIDI_SysEx_NSX39();
-            $nsx39->dumpPayload(substr($payload, $sysExPrefixLen));
+        static $sysExVendors = null;
+        if (is_null($sysExVendors)) {
+            $dir = dirname(__FILE__);
+            $d = opendir("$dir/SysEx");
+            while ($file = readdir($d)) {
+                if (substr($file, -4, 4) === ".php") {
+                    $vendor = substr($file, 0, -4);
+                    $sysExVendors []= $vendor;
+                    require_once("IO/MIDI/SysEx/$vendor.php");
+                }
+            }
+        }
+        foreach ($sysExVendors as $vendor) {
+            $klass = "IO_MIDI_SysEx_$vendor";
+            $sysExPrefix = $klass::SYSEX_PREFIX;
+            $sysExPrefixLen = strlen($sysExPrefix);
+            if (strncmp($payload, $sysExPrefix, $sysExPrefixLen) === 0) {
+                $vendersys = new $klass();
+                $vendersys->dumpPayload(substr($payload, $sysExPrefixLen));
+           }
         }
     }
 }
